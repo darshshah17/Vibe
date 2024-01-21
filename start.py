07d7200092d0dcd2e41c3028fa5e633a21daa9bc
darshshah17 <78572580+darshshah17@users.noreply.github.com>
2024-01-20 23:21:53 -0500

    Add files via upload

---
+++ start.py
@@ -0,0 +1,34 @@
+import whisper
+
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+# Preprocess text (username and link placeholders)
+
+audio_path = 'audios/plswork.mp3'
+
+m = whisper.load_model("small")
+result = m.transcribe(audio_path)
+transcript = result["text"]
+
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+# PT
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+#model.save_pretrained(MODEL)
+audio_path = 'sanskritidepression.mp3'
+
+encoded_input = tokenizer(transcript, return_tensors='pt')
+output = model(**encoded_input)
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+ranking = np.argsort(scores)
+ranking = ranking[::-1]
+for i in range(scores.shape[0]):
+    l = config.id2label[ranking[i]]
+    s = scores[ranking[i]]
+    print(f"{i+1}) {l} {np.round(float(s), 4)}")
